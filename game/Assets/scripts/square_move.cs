78a843c97788bc69e46afb6d350649ba963e7d72
maximantonovich <antonovich.maxim@gmail.com>
2026-07-01 22:11:48 +0300

    добавил спрайты и анимации

--- game/Assets/scripts/square_move.cs
+++ game/Assets/scripts/square_move.cs
@@ -9,17 +9,20 @@ public class square_move : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 move;
-
+    private Animator _animator;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
-    
+        _animator.SetFloat("Vertical", move.y);
+        _animator.SetFloat("Horizontal", move.x);
+        _animator.SetFloat("Speed", move.sqrMagnitude);
     }
 
     private void FixedUpdate()

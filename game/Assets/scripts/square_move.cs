f14b3b57578e24b8ba1566af2df1f33aa777e812
maximantonovich <antonovich.maxim@gmail.com>
2026-07-01 17:25:31 +0300

    научил квадрат ходить на wasd и стрелочки

---
+++ game/Assets/scripts/square_move.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+
+public class square_move : MonoBehaviour
+{
+
+    [SerializeField] private float moving_speed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 move;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = move * moving_speed;
+    }
+}
